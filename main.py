7a44082b715fc3ff3de95b75db9bd2ecf67e99eb
viktor1979-19 <polivitek@gmail.com>
2024-09-07 23:32:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print(12 + 89 - 1)
+# "1st program".
+print(9 ** 0.5 * 5)
+# "2nd program".
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program".
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print( 6 == 8)
+# "4th program".
+print('123.456')
+print(float('123.456' ))
+print(123.456 * 10)
+print(int(1234.56))
+print(1234 % 10)
