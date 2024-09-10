1b680538348e155f3fe0fd4d699aea5f1eec4ae5
viktor1979-19 <polivitek@gmail.com>
2024-09-11 00:16:54 +0300

    Add files via upload

--- dz3.py
+++ dz3.py
@@ -1,5 +1,5 @@
 number_of_homework = 12
 number_of_hours_spent = 1.5
 course_name = "Pithon"
-time_for_one_task = (1.5/12)
+time_for_one_task = number_of_hours_spent / number_of_homework
 print("Курс:",course_name,"всего задач:",number_of_homework,"затрачено часов:",number_of_hours_spent,"среднее время выполнения:",time_for_one_task)
