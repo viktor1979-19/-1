31959a93cbc817744dc1c07ccec3f4330df863a4
viktor1979-19 <polivitek@gmail.com>
2025-01-20 00:15:00 +1000

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,50 @@
+# Задача "Банковские операции":
+import threading
+import random
+import time
+from threading import Thread, Lock
+
+class Bank(Thread):
+
+    def __init__(self):
+        super().__init__()
+        self.balance = 0
+        self.lock = Lock()
+
+# 1 Поток "депозит"
+    def deposit(self):
+        for i in range(100):
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            y = random.randint(50, 500)
+            self.balance += y
+            print(f'Пополнение: {y}. Баланс: {self.balance}')
+
+            # ожидание в 0.001 секунды, имитация скорости выполнения пополнения.
+            time.sleep(0.001)
+
+# 2й поток "Брать"
+    def take(self):
+        for i in range(100):
+            x = random.randint(50,500)
+            print(f'Запрос на {x}')
+            if self.balance >= x:
+                self.balance -= x
+                print(f'Снятие: {x}. Баланс: {self.balance}')
+            else:
+                print(f'Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+            time.sleep(0.001)
+
+
+bk = Bank()
+
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
