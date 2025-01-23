e4216b77d70cc4d1104bfab6acd53678f60e3b93
viktor1979-19 <polivitek@gmail.com>
2025-01-23 19:18:43 +1000

    Add files via upload

--- module_12_1.py
+++ module_12_1.py
@@ -3,21 +3,21 @@ import unittest
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
-        walker = runner.Runner('Вася')
+        walker = runner.Runner('Саня')
         # for i in range(9):
         for i in range(10):
             walker.walk()
         self.assertEqual(walker.distance, 50)
 
     def test_run(self):
-        run = runner.Runner('Петя')
+        run = runner.Runner('Митя')
         for i in range(10):
             run.run()
         self.assertEqual(run.distance, 100)
 
     def test_challenge(self):
-        walker2 = runner.Runner('Маша')
-        run2 = runner.Runner('Саша')
+        walker2 = runner.Runner('Маня')
+        run2 = runner.Runner('Глаша')
         for i in range(10):
             walker2.walk()
             run2.run()
